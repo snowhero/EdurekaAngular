4c0471b94a7191a569fdfcf88dfcd5fc799adcb4
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2019-08-28 14:16:54 -0700

    Edureka Angular continuation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    setTimeout(() => {
+      this.buttonState = false;
+    }, 2500);
+  }
   name = 'Hiroyuki';
+  serverName = 'Apollo';
+  serverStatus = 'offline';
+  serverPID = 11;
+  statusFlag = false;
+  buttonState = true;
+
+  toggleServerStatus() {
+    this.statusFlag = !this.statusFlag;
+    if(this.statusFlag == true) {
+      this.serverStatus = 'online';
+    }
+    else this.serverStatus = 'offline';
+    return this.serverStatus;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { SubServerComponent } from './sub-server/sub-server.component';
-import { RedComponent } from './red/red.component';
-import { GreenComponent } from './green/green.component';
-import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    SubServerComponent,
-    RedComponent,
-    GreenComponent,
-    YellowComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
