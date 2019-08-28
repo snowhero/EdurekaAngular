1384c19ec2cb5897eada8e4467c2a15f64bb18fb
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2019-08-28 13:20:27 -0700

    Edureka assignment 1 red, green, yellow components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { SubServerComponent } from './sub-server/sub-server.component';
+import { RedComponent } from './red/red.component';
+import { GreenComponent } from './green/green.component';
+import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    SubServerComponent,
+    RedComponent,
+    GreenComponent,
+    YellowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
